09dd4526188bf3d077bfae78319cc911a12a95ce
Abipyth <abi18june99@gmail.com>
2024-06-29 12:46:42 +0530

    some spelling mistakes and corrections are handled using vscode

--- pms/app/signals.py
+++ pms/app/signals.py
@@ -1,10 +1,9 @@
 from .models import APIkey
 from django.db.models.signals import post_save
-from django.dispatch import @receiver
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 @receiver(post_save, sender=User)
 def create_api_key(sender,instance,created,**kwargs):
   if created:
     APIkey.objects.create(user=instance)
-  
