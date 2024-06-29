bacb101df76f14231f05d2e790e67182c7999e4c
Abipyth <abi18june99@gmail.com>
2024-06-29 12:22:49 +0530

    models.py added with new class for API key based authentication system process

--- pms/app/models.py
+++ pms/app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+import binascii
+import os
 
 class Product(models.Model):
     name=models.CharField(max_length=25)
@@ -8,3 +10,17 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class APIkey(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    key=models.CharField(max_length=40, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key=self.generate_key()
+        return super().save(*args, **kwargs)
+    
+    def generate_key(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+    
+    def __str__(self):
+        return f'{self.user.username}-{self.key}'
